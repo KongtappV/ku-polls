e86793b73d4568ce37afc859313f95aa7f25118b
KongtappV <Kongtapp.v@ku.th>
2020-11-02 01:01:16 +0700

    add logging for login&logout

--- kupsite/polls/views.py
+++ kupsite/polls/views.py
@@ -6,14 +6,17 @@ from django.views import generic
 from django.utils import timezone
 
 from django.contrib.auth.decorators import login_required
+from django.dispatch import receiver
+from django.contrib.auth.signals import user_logged_in, user_logged_out, user_login_failed
 from django.contrib import messages
 
 from .models import Choice, Question, Vote
 
 import logging
+from datetime import datetime
 
 log = logging.getLogger("polls")
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO, format="")
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
@@ -90,8 +93,32 @@ def vote(request, question_id):
             choice.votes = Vote.objects.filter(question=question).filter(choice=choice).count()
             choice.save()
         
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
         messages.success(request, "Your choice successfully recorded. Thank you.")
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        _ip = x_forwarded_for.split(',')[0]
+    else:
+        _ip = request.META.get('REMOTE_ADDR')
+    return _ip
+
+@receiver(user_logged_in)
+def get_ip_login(sender, request, user, **kwargs):
+    ip = get_client_ip(request)
+    date = datetime.now()
+    log.info('Login user(success): %s , IP: %s , Date: %s', request.user.username , ip, str(date))
+
+@receiver(user_logged_out)
+def get_ip_logged_out(sender, request, user, **kwargs):
+    ip = get_client_ip(request)
+    date = datetime.now()
+    log.info('Logout user(success): %s , IP: %s , Date: %s', request.user.username , ip, str(date))
+
+@receiver(user_login_failed)
+def get_ip_login_failed(sender, request, user, **kwargs):
+    ip = get_client_ip(request)
+    date = datetime.now()
+    log.warning('Login user(failure): %s , IP: %s , Date: %s', request.user.username , ip, str(date))
